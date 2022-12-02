a57efee07d661d845df2ffc06524651f62dfc69e
KelvinMarcano <kelvin.marcano@simone.com.ve>
2022-12-02 13:39:22 -0400

    Limpieza de carpeta de requisicion cabecera en proceso

--- WEB-INF/action/smn_proceso/smn_requisicion_cabecera/filter/smn_requisicion_detalle1/filter/editor/getlineactf/query.sql
+++
@@ -1,4 +0,0 @@
-select DISTINCT  smn_base.smn_activo_fijo.smn_afijo_id as id, smn_base.smn_activo_fijo.acf_codigo|| ' - ' ||smn_base.smn_activo_fijo.acf_nombre as item from smn_base.smn_activo_fijo
-inner join smn_compras.smn_rel_linea_afijo on smn_compras.smn_rel_linea_afijo.smn_afijo_id = smn_base.smn_activo_fijo.smn_afijo_id
-where 
-smn_compras.smn_rel_linea_afijo.smn_lineas_id=${fld:id}
--- WEB-INF/action/smn_proceso/smn_requisicion_cabecera/filter/smn_requisicion_detalle1/filter_old/search/clause-smn_linea_id.sql
+++
@@ -1,2 +0,0 @@
- and
- 	smn_compras.smn_requisicion_detalle.smn_linea_id=${fld:smn_linea_id}
